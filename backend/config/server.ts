3c9c312edf91cc2c5600efed30146c123411a2a9
Sailaja Adapa <adapasailaja17@gmail.com>
2025-02-15 16:53:51 +0530

    Updated Strapi

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,7 +1,13 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+module.exports = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
+  },
+  settings: {
+    cors: {
+      enabled: true,
+      origin: ["http://localhost:3000"], // Add your frontend URL
+    },
   },
 });
